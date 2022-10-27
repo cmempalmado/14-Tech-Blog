f43f391ebde6194cad934c032b4865942ec3e14c
cmempalmado <cmempalmado@gmail.com>
2022-10-27 06:50:29 -0700

    homepage now renders

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
+// const { User } = require('../models/');
 
 router.get('/', async (req, res) => {
-    try {
-      const postData = await Post.findAll({
-        include: [User],
-      });
-  
-      const posts = postData.map((post) => post.get({ plain: true }));
-  
-      res.render('all-posts', { posts });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  res.render('login');
+});
+
+
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User');
+const Post = require('./Post');
+
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
+  });
+
+module.exports = {User, Post};
